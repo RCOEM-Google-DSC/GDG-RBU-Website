1182b29172c515c07a960b7f991edaab7ff8cd6e
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2025-02-05 23:17:01 +0530

    feat: 🔧 Modify the Admin Panel Schema & file Str

---
+++ app/(admin-control)/admin/user/page.tsx
@@ -0,0 +1,6 @@
+// - Display a table with user details: name, email, role, image
+// - Add a search bar to find users by name
+// - Add a dropdown to filter users by role
+// - List all users with their name, email, role, and image in a table
+// - Include a delete button to remove a user
+// - Include a toggle button to edit a user's role
