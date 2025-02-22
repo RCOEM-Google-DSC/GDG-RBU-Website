6ab32f6c2c44aacccd9bd0fb87b6864a0ebcb86f
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2025-02-22 19:38:14 +0530

    feat(database): modify storage

--- utils/supabase/ウイルス/スキーマ.sql
+++ utils/supabase/ウイルス/スキーマ.sql
@@ -323,3 +323,12 @@ VALUES ('profile', 'profile', true);
 UPDATE storage.buckets
 SET public = true
 WHERE id = 'profile';
+
+-- Create the bucket
+INSERT INTO storage.buckets (id, name, public)
+VALUES ('events', 'events', true);
+
+-- Ensure the bucket is public
+UPDATE storage.buckets
+SET public = true
+WHERE id = 'events';
