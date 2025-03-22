9a902b34127c68664d5dc1878019be37483b010c
MasterAbhayMishra <abhayansh.mishra1371@gmail.com>
2025-03-23 01:24:49 +0530

    fixed the font

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${GeistSans.className} dark`}
+      className={`dark`}
       suppressHydrationWarning
     >
       <body className="min-h-screen bg-black text-foreground">
